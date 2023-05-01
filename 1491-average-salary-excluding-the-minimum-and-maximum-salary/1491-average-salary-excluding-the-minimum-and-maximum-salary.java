290fe73d60cae65305e35ea6f2bbfee1b518f02a
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-05-01 21:57:32 +0300

    Time: 0 ms (100.00%), Space: 40.4 MB (52.51%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -1,13 +1,12 @@
 class Solution {
     public double average(int[] salary) {
-        final DecimalFormat df = new DecimalFormat("0.00000");
         double res=0.0;
         Arrays.sort(salary);
         for(int i=1;i<salary.length-1;i++){
             res += salary[i];
             
         }
-        return Double.valueOf(df.format(res/(salary.length-2)));
+        return Double.valueOf(res/(salary.length-2));
         
     }
 }
