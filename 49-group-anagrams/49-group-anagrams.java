916a359fb3e23e4342c76f2101eb4437f9bf5f7d
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-05-23 06:04:39 +0300

    Time: 7 ms (74.86%), Space: 47.1 MB (16.14%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,19 +1,38 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
-        List<List<String>> values = new ArrayList<>();
         for(String i: strs){
-            int[] hash = new int[26];
-            for(char c: i.toCharArray()){
-                hash[c - 'a']++;
-            }
-            String key = Arrays.toString(hash);
+            char[] ca = i.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
             map.computeIfAbsent(key, k -> new ArrayList<>());
             map.get(key).add(i);
         }
-        values.addAll(map.values());
-        return values;
+        return new ArrayList<>(map.values());
         
     }
     
 }
+
+//Another solution but takes more time
+
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         Map<String, List<String>> map = new HashMap<>();
+//         List<List<String>> values = new ArrayList<>();
+//         for(String i: strs){
+//             int[] hash = new int[26];
+//             for(char c: i.toCharArray()){
+//                 hash[c - 'a']++;
+//             }
+//             String key = Arrays.toString(hash);
+//             map.computeIfAbsent(key, k -> new ArrayList<>());
+//             map.get(key).add(i);
+//         }
+//         values.addAll(map.values());
+//         return values;
+        
+//     }
+    
+// }
