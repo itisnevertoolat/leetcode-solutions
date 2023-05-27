2649b8de4619f523de6b171d50690fe22b8865f3
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-05-27 11:47:06 +0300

    Time: 10 ms (89.32%), Space: 44.4 MB (83.57%) - LeetHub

--- 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.java
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.java
@@ -1,16 +1,16 @@
 class Solution {
     public String makeSmallestPalindrome(String s) {
+        char[] palindrome = s.toCharArray();
         int left = 0;
         int right = s.length()-1;
-        StringBuilder res = new StringBuilder(s);
         while(left < right){
-            if(s.charAt(left) > s.charAt(right)){
-                res.setCharAt(left, s.charAt(right));
-            }else if(s.charAt(left) < s.charAt(right))  res.setCharAt(right, s.charAt(left));
+            if(palindrome[left] > palindrome[right]){
+                palindrome[left] = palindrome[right];
+            }else if(palindrome[left] < palindrome[right])  palindrome[right] = palindrome[left];
             left++;
             right--;
             
         }
-        return res.toString();
+        return new String(palindrome);
     }
 }
