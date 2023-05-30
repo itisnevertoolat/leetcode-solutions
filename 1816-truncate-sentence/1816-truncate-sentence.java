b9e1e0db3fef41f69aeae83417d3e7ced628d045
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-05-30 13:13:55 +0300

    Time: 0 ms (100.00%), Space: 40.8 MB (80.69%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.java
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -1,24 +1,12 @@
-// class Solution {
-//     public String truncateSentence(String s, int k) {
-//         String[] arr = s.split(" ");
-//         StringBuilder res=new StringBuilder();
-//         for(int i=0;i<k-1;i++){
-//             res.append(arr[i]+" ");
-//         }
-//         res.append(arr[k-1]);
-//         return res.toString();
-//     }
-// }
-
 class Solution {
     public String truncateSentence(String s, int k) {
         int track=0;
-        StringBuilder res=new StringBuilder();
+        int i =0;
         for(char c: s.toCharArray()){
             if(c == ' ') track++;
-            if(track==k) break;
-            res.append(c);
+            if(track==k) return s.substring(0,i);
+            i++;
         }
-        return res.toString();
+        return s;
     }
 }
