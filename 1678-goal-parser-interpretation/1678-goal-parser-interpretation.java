78ca71d71f666f3a895d0a0750737470dbf1b947
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-05-31 18:46:42 +0300

    Time: 1 ms (71.56%), Space: 40.7 MB (71.10%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder res = new StringBuilder();
+        for(int i=0;i<command.length();i++){
+            if(command.charAt(i) == ')' && command.charAt(i-1) == '(') res.append("o");
+            else if(command.charAt(i) == ')' && command.charAt(i-1) == 'l') res.append("al");
+            else if(command.charAt(i) == 'G') res.append("G");
+            
+        }
+        return res.toString();
+    }
+}
