5e1a3ebd7dc5708162bd8a4b738a1fbea4d295b0
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-05-31 18:48:30 +0300

    Time: 1 ms (71.56%), Space: 40.6 MB (71.10%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,12 +1,19 @@
+// class Solution {
+//     public String interpret(String command) {
+//         StringBuilder res = new StringBuilder();
+//         for(int i=0;i<command.length();i++){
+//             if(command.charAt(i) == ')' && command.charAt(i-1) == '(') res.append("o");
+//             else if(command.charAt(i) == ')' && command.charAt(i-1) == 'l') res.append("al");
+//             else if(command.charAt(i) == 'G') res.append("G");
+            
+//         }
+//         return res.toString();
+//     }
+// }
+
+
 class Solution {
     public String interpret(String command) {
-        StringBuilder res = new StringBuilder();
-        for(int i=0;i<command.length();i++){
-            if(command.charAt(i) == ')' && command.charAt(i-1) == '(') res.append("o");
-            else if(command.charAt(i) == ')' && command.charAt(i-1) == 'l') res.append("al");
-            else if(command.charAt(i) == 'G') res.append("G");
-            
-        }
-        return res.toString();
+        return command.replace("()", "o").replace("(al)", "al");
     }
 }
