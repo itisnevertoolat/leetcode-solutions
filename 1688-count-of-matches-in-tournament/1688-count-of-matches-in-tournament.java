73a118cfda3b417ebbb6cb13bbe647e11ec12f0e
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-06-05 08:52:11 +0300

    Time: 0 ms (100.00%), Space: 39.4 MB (79.82%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -1,21 +1,29 @@
-class Solution {
-    int matches = 0;
-    int teams = 0;
-    public int numberOfMatches(int n) {
-        if (n == 1){
-            return matches;
-        }
-        if(n % 2 ==0){
-            teams = n / 2;
-            matches += n / 2;
-        }
+// class Solution {
+//     int matches = 0;
+//     int teams = 0;
+//     public int numberOfMatches(int n) {
+//         if (n == 1){
+//             return matches;
+//         }
+//         if(n % 2 ==0){
+//             teams = n / 2;
+//             matches += n / 2;
+//         }
             
-        else{
-            teams = (n - 1) / 2 + 1;
-            matches += (n - 1) / 2;
-            }
+//         else{
+//             teams = (n - 1) / 2 + 1;
+//             matches += (n - 1) / 2;
+//             }
         
-        numberOfMatches(teams);
-        return matches;
+//         numberOfMatches(teams);
+//         return matches;
+//     }
+// }
+
+
+//another easy beasy solution
+class Solution {
+    public int numberOfMatches(int n) {
+        return n-1;
     }
 }
