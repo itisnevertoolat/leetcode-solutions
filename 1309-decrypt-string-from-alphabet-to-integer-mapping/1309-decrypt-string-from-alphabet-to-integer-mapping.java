ef236002b5f457111d1d755f135aa38c27e1ae1b
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-06-14 12:53:07 +0300

    Time: 6 ms (38.38%), Space: 41.2 MB (50.81%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -1,7 +1,6 @@
 class Solution {
     public String freqAlphabets(String s) {
         StringBuilder sb = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
 
         for(int i=s.length() - 1 ;i >= 0;i--){
             if(s.charAt(i) == '#'){
@@ -11,9 +10,8 @@ class Solution {
                  sb.append((char)('a' +Integer.parseInt(s.charAt(i)+"")-1));
             }
         }
-        for(int i=sb.length() - 1 ;i >= 0;i--){
-            sb2.append(sb.charAt(i));
-        }
-        return sb2.toString();
+         sb.reverse();
+
+        return sb.toString();
     }
 }
