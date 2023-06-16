58c9391b3be76d1fd04dca1d14eb73391bdd5473
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-06-16 20:10:28 +0300

    Time: 1 ms (100.00%), Space: 48.3 MB (81.70%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -1,31 +1,42 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-
 class Solution {
-    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-        Queue<TreeNode> orgQueue = new LinkedList<>();
-        Queue<TreeNode> cloQueue = new LinkedList<>();
-        orgQueue.add(original);
-        cloQueue.add(cloned);
-        TreeNode res = new TreeNode();
-        TreeNode node;
-        while(!orgQueue.isEmpty()){
-            node = orgQueue.remove();
-            res = cloQueue.remove();
-            if(node == target) return res;
-            if(node.left != null) orgQueue.add(node.left);
-            if(node.right != null) orgQueue.add(node.right);
-            if(res.left != null) cloQueue.add(res.left);
-            if(res.right != null) cloQueue.add(res.right);
-            
+public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if(original == target || original == null) {
+            return cloned;
         }
-        return res;
-    }
+        
+        TreeNode oleft = original.left;
+        TreeNode cleft = cloned.left;
+        TreeNode left = getTargetCopy(oleft, cleft, target);
+        if(left != null)   //Found the target node in left subtree.
+            return left;
+        
+		//Target node is not in left subtree, then it must be in right subtree.
+        TreeNode oright = original.right;
+        TreeNode cright = cloned.right;
+        TreeNode right = getTargetCopy(oright, cright, target);
+        
+        return right;
 }
+    }
+
+// class Solution {
+//     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+//         Queue<TreeNode> orgQueue = new LinkedList<>();
+//         Queue<TreeNode> cloQueue = new LinkedList<>();
+//         orgQueue.add(original);
+//         cloQueue.add(cloned);
+//         TreeNode res = new TreeNode();
+//         TreeNode node;
+//         while(!orgQueue.isEmpty()){
+//             node = orgQueue.remove();
+//             res = cloQueue.remove();
+//             if(node == target) return res;
+//             if(node.left != null) orgQueue.add(node.left);
+//             if(node.right != null) orgQueue.add(node.right);
+//             if(res.left != null) cloQueue.add(res.left);
+//             if(res.right != null) cloQueue.add(res.right);
+            
+//         }
+//         return res;
+//     }
+// }
