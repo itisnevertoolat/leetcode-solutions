f53a27f32e9a912fbee6b172f0f523ac071ac57b
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-06-17 08:43:08 +0300

    Time: 11 ms (21.13%), Space: 43.9 MB (56.60%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.java
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.java
@@ -1,5 +1,14 @@
 class Solution {
     public String removeTrailingZeros(String num) {
+        return num.replaceAll("0+$", "");
+    }
+}
+
+
+//using substring
+
+/*class Solution {
+    public String removeTrailingZeros(String num) {
         int idx = 0;
         for(int i=0;i< num.length();i++){
             if(num.charAt(i) != '0') idx=i;
@@ -9,15 +18,20 @@ class Solution {
         return num.substring(0, idx+1);
     }
 }
+*/
 
-// class Solution {
-//     public String removeTrailingZeros(String num) {
-//         StringBuilder s = new StringBuilder();
-//         for(int i=num.length()-1;i>=0;i--){
-//             if(s.length() == 0 && num.charAt(i) == '0') continue;
-//             else s.append(num.charAt(i));
-//         }
-//         s.reverse();
-//         return s.toString();
-//     }
-// }
+// using stringBuilder
+
+/*
+class Solution {
+    public String removeTrailingZeros(String num) {
+        StringBuilder s = new StringBuilder();
+        for(int i=num.length()-1;i>=0;i--){
+            if(s.length() == 0 && num.charAt(i) == '0') continue;
+            else s.append(num.charAt(i));
+        }
+        s.reverse();
+        return s.toString();
+    }
+}
+*/
