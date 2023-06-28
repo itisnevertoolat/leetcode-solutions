2cac12d42119c46b7e3a368ec66a76ea3a241d14
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-06-29 00:41:13 +0300

    Time: 1 ms (100.00%), Space: 44.1 MB (35.93%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -1,12 +1,25 @@
+// class Solution {
+//     public int minOperations(int[] nums) {
+//         int res = 0;
+//         for(int i=0;i< nums.length-1;i++){
+//             int pre = nums[i+1];
+//             if(nums[i+1] <= nums[i]){
+//                 int n = (nums[i] - nums[i+1]) + 1;
+//                 res += n;
+//                 nums[i+1] = nums[i+1] + n; 
+//             } 
+//         }
+//         return res;
+//     }
+// }
+
 class Solution {
     public int minOperations(int[] nums) {
         int res = 0;
-        for(int i=0;i< nums.length-1;i++){
-            if(nums[i+1] <= nums[i]){
-                int n = (nums[i] - nums[i+1]) + 1;
-                res += n;
-                nums[i+1] = nums[i+1] + n; 
-            } 
+        int prev= 0;
+        for(int i: nums){
+            if(i <= prev) res += ++prev- i;
+            else prev = i;
         }
         return res;
     }
