d090b514460a6864e6e09b1d68bb4eecb9d8ce50
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-07-09 18:02:41 +0300

    Time: 0 ms (100.00%), Space: 39.8 MB (21.90%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,25 +1,5 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        StringBuilder s = new StringBuilder();
-        StringBuilder s1 = new StringBuilder();
-        long i;
-        s.append("0");
-        s1.append("0");
-        for (i = 1 << 30; i > 0; i = i / 2)
-        {
-          if((x & i) != 0) s.append("1");
-          else s.append("0");
-        }
-        for (i = 1 << 30; i > 0; i = i / 2)
-        {
-          if((y & i) != 0) s1.append("1");
-          else s1.append("0");
-        }
-        int res = 0;
-
-        for(int k=0;k<32;k++)
-          if(s.charAt(k) != s1.charAt(k)) res++;
-
-       return res;
+        return Integer.bitCount(x^y);
     }
 }
