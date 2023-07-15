0b164620d3e7542cec8c7c768b95b0dc8c630f38
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-07-15 15:49:13 +0300

    Time: 0 ms (100.00%), Space: 40.9 MB (36.70%) - LeetHub

--- 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.java
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.java
@@ -1,18 +1,40 @@
 class Solution {
     public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
-        StringBuilder fN = new StringBuilder();
-        StringBuilder sN = new StringBuilder();
-        StringBuilder tN = new StringBuilder();
-        for(int i=0;i<firstWord.length();i++){
-            fN.append(Math.abs(firstWord.charAt(i) - 'a'));
+        int sumfirst=0, sumsecond=0, sumtarget=0;
+        for(char c : firstWord.toCharArray()){
+            sumfirst += c-'a';
+            sumfirst *= 10;
         }
-        for(int i=0;i<secondWord.length();i++){
-            sN.append(Math.abs(secondWord.charAt(i) - 'a'));
+        for(char c : secondWord.toCharArray()){
+            sumsecond += c-'a';
+            sumsecond *= 10;
         }
-        for(int i=0;i<targetWord.length();i++){
-            tN.append(Math.abs(targetWord.charAt(i) - 'a'));
+        for(char c : targetWord.toCharArray()){
+            sumtarget += c-'a';
+            sumtarget *= 10;
         }
-        return (Integer.parseInt(fN.toString())+Integer.parseInt(sN.toString()) == Integer.parseInt(tN.toString()));
-        
+       
+        return (sumfirst + sumsecond) == sumtarget;
     }
 }
+
+
+
+// class Solution {
+//     public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+//         StringBuilder fN = new StringBuilder();
+//         StringBuilder sN = new StringBuilder();
+//         StringBuilder tN = new StringBuilder();
+//         for(int i=0;i<firstWord.length();i++){
+//             fN.append(Math.abs(firstWord.charAt(i) - 'a'));
+//         }
+//         for(int i=0;i<secondWord.length();i++){
+//             sN.append(Math.abs(secondWord.charAt(i) - 'a'));
+//         }
+//         for(int i=0;i<targetWord.length();i++){
+//             tN.append(Math.abs(targetWord.charAt(i) - 'a'));
+//         }
+//         return (Integer.parseInt(fN.toString())+Integer.parseInt(sN.toString()) == Integer.parseInt(tN.toString()));
+        
+//     }
+// }
