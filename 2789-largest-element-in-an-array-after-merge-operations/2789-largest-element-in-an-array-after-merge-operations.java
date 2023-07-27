f4d5207367dff0023cf7ef6e5f1bd3a17ea92fbe
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-07-27 21:01:34 +0300

    Time: 6 ms (28.81%), Space: 56.5 MB (88.12%) - LeetHub

---
+++ 2789-largest-element-in-an-array-after-merge-operations/2789-largest-element-in-an-array-after-merge-operations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long maxArrayValue(int[] arr) {
+        int end = arr.length - 1;
+        int bEnd = arr.length - 2;
+        long sum = 0L;
+        long[] arr2 = new long[arr.length];
+        for(int i=0;i<arr.length;i++)
+            arr2[i] = (long) arr[i];
+            
+        for(int i=0;i< arr2.length-1;i++){
+            if(arr2[end] >= arr2[bEnd]){
+                sum = arr2[end] + arr2[bEnd];
+                arr2[bEnd] = sum;
+            }
+            else
+                sum = 0;
+
+            end--;
+            bEnd--;
+                    }
+        Arrays.sort(arr2);
+        return arr2[arr2.length-1];
+        
+        
+    }
+}
