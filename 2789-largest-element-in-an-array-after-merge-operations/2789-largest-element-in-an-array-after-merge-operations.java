cbcd9ea5a723a92e563867eae39616c2d4cb70ff
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-07-27 21:12:50 +0300

    Time: 1 ms (100.00%), Space: 57.9 MB (64.97%) - LeetHub

--- 2789-largest-element-in-an-array-after-merge-operations/2789-largest-element-in-an-array-after-merge-operations.java
+++ 2789-largest-element-in-an-array-after-merge-operations/2789-largest-element-in-an-array-after-merge-operations.java
@@ -1,26 +1,44 @@
 class Solution {
     public long maxArrayValue(int[] arr) {
-        int end = arr.length - 1;
-        int bEnd = arr.length - 2;
         long sum = 0L;
-        long[] arr2 = new long[arr.length];
-        for(int i=0;i<arr.length;i++)
-            arr2[i] = (long) arr[i];
             
-        for(int i=0;i< arr2.length-1;i++){
-            if(arr2[end] >= arr2[bEnd]){
-                sum = arr2[end] + arr2[bEnd];
-                arr2[bEnd] = sum;
-            }
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i] <= sum)
+                sum += arr[i];
             else
-                sum = 0;
-
-            end--;
-            bEnd--;
-                    }
-        Arrays.sort(arr2);
-        return arr2[arr2.length-1];
+                sum = arr[i];
+        }
+        return sum;
         
         
     }
 }
+
+
+
+// class Solution {
+//     public long maxArrayValue(int[] arr) {
+//         int end = arr.length - 1;
+//         int bEnd = arr.length - 2;
+//         long sum = 0L;
+//         long[] arr2 = new long[arr.length];
+//         for(int i=0;i<arr.length;i++)
+//             arr2[i] = (long) arr[i];
+            
+//         for(int i=0;i< arr2.length-1;i++){
+//             if(arr2[end] >= arr2[bEnd]){
+//                 sum = arr2[end] + arr2[bEnd];
+//                 arr2[bEnd] = sum;
+//             }
+//             else
+//                 sum = 0;
+
+//             end--;
+//             bEnd--;
+//                     }
+//         Arrays.sort(arr2);
+//         return arr2[arr2.length-1];
+        
+        
+//     }
+// }
