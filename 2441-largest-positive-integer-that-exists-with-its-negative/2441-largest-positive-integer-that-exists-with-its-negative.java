110ae8b84353858f71130c33d1f49aa4ebc75c3e
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-08-17 10:02:54 +0300

    Time: 5 ms (88.98%), Space: 43.5 MB (83.81%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
@@ -4,9 +4,10 @@ class Solution {
         int start = 0;
         int end  = nums.length - 1;
         while(end > start){
-            if(-1*nums[start] == nums[end])
+            int x = -1 * nums[start];
+            if(x == nums[end])
                 return nums[end];
-            if(-1*nums[start] > nums[end])
+            if(x > nums[end])
                 start++;
             else
                 end--;
