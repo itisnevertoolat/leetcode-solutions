801f10af2abe3a1c484114dbc0b3ebf77999a961
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-08-22 09:31:21 +0300

    Time: 3 ms (21.53%), Space: 41.1 MB (59.76%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -1,30 +1,17 @@
 class Solution {
-        public int countPairs(List<Integer> nums, int target) {
-            Collections.sort(nums); //sorting an array
-            int res=0, i=0, j=nums.size()-1;
-            while (i<j){
-                int a = nums.get(i), b = nums.get(j);
-                if (a+b>=target) {
-                    j--; //if lowest+highest is too much, we reduce the highest.
-                } else {
-                    res+=j-i; //[1,2,3] t=5. then we have 3 (j-i) pairs: (0,1), (0,2);
-                    i++;      
-                }
+    
+    public int countPairs(List<Integer> nums, int target) {
+        int res = 0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums.get(i) + nums.get(j) < target)
+                    res++;
             }
+        }
+        
+        
         return res;
     }
-//     public int countPairs(List<Integer> nums, int target) {
-//         int res = 0;
-//         for(int i=0;i<nums.size();i++){
-//             for(int j=i+1;j<nums.size();j++){
-//                 if(nums.get(i) + nums.get(j) < target)
-//                     res++;
-//             }
-//         }
-        
-        
-//         return res;
-//     }
 }
 // class Solution {
 //     public int countPairs(List<Integer> nums, int target) {
