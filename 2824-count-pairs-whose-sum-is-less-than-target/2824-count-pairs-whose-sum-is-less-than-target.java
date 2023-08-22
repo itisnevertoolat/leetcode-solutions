4169325accf0bce717693aa3cb2ba0ef74686252
Muhammed Ahmed <131061525+itisnevertoolate@users.noreply.github.com>
2023-08-22 09:28:13 +0300

    Time: 2 ms (98.64%), Space: 41.1 MB (59.76%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -1,18 +1,32 @@
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
-        Collections.sort(nums);
-        int start = 0;
-        int end = nums.size() -1 ;
         int res = 0;
-        while(end > start){
-            if(nums.get(start) + nums.get(end) < target){
-                res += end - start;
-                start++;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums.get(i) + nums.get(j) < target)
+                    res++;
             }
-            else
-                end--;
         }
+        
         
         return res;
     }
 }
+// class Solution {
+//     public int countPairs(List<Integer> nums, int target) {
+//         Collections.sort(nums);
+//         int start = 0;
+//         int end = nums.size() -1 ;
+//         int res = 0;
+//         while(end > start){
+//             if(nums.get(start) + nums.get(end) < target){
+//                 res += end - start;
+//                 start++;
+//             }
+//             else
+//                 end--;
+//         }
+        
+//         return res;
+//     }
+// }
