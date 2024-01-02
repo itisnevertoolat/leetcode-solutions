5f7295df001516cd725a0e7ab32e0288cf8588d1
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-01-02 23:02:18 +0200

    Time: 7 ms (59.34%), Space: 45.5 MB (5.91%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -1,22 +1,15 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
-        Map<Character, Integer> map = new HashMap<>();
-        int res = 0;
-        for(Character c: chars.toCharArray())
-            map.put(c, map.getOrDefault(c, 0)+1);
-        for(String s: words){
-            boolean good = true;
-            Map<Character, Integer> map2 = new HashMap<>();
-            for(Character c: s.toCharArray())
-                map2.put(c, map2.getOrDefault(c, 0)+1);
-            for(Character c: s.toCharArray()){
-                if(map.getOrDefault(c, 0) < map2.get(c)){
-                    good = false;
-                    break;
-                }
-            }
-            res += good == true ? s.length() : 0;
+        int cnt[] = new int[26], ans = 0;
+        chars.chars().forEach(c -> ++cnt[c - 'a']); // count chars.
+outer: 
+        for (String w : words) {
+            int[] count = cnt.clone();
+            for (char c : w.toCharArray())
+                if (--count[c - 'a'] < 0) // not enough, continue next word in words.
+                    continue outer;
+            ans += w.length();
         }
-        return res;
+        return ans;
     }
 }
