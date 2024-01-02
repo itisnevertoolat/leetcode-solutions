d90995cf41a92ea2450be872e403176b675eadae
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-01-02 22:57:49 +0200

    Time: 102 ms (5.12%), Space: 45.3 MB (5.91%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -7,9 +7,9 @@ class Solution {
         for(String s: words){
             boolean good = true;
             Map<Character, Integer> map2 = new HashMap<>();
-            for(char c: s.toCharArray())
+            for(Character c: s.toCharArray())
                 map2.put(c, map2.getOrDefault(c, 0)+1);
-            for(char c: s.toCharArray()){
+            for(Character c: s.toCharArray()){
                 if(map.getOrDefault(c, 0) < map2.get(c)){
                     good = false;
                     break;
