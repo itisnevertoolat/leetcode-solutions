5937563bfe061765603fd5589de0e83553c10482
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-01-02 10:51:16 +0200

    Time: 1 ms (79.52%), Space: 43.2 MB (70.34%) - LeetHub

---
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countTestedDevices(int[] batteryPercentages) {
+        int count = 0;
+        for(int i: batteryPercentages){
+            if(i - count > 0){
+                    count++;
+            }
+        }
+        return count;
+    }
+}
