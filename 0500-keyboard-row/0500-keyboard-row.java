631d56f6e4be1b85fbac7815f3160553a3b90462
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-01-09 00:30:38 +0200

    Time: 0 ms (100.00%), Space: 41.2 MB (31.52%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -1,7 +1,30 @@
 class Solution {
-       public String[] findWords(String[] words) {
-            return Stream.of(words)
-                .filter(w -> w.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*"))
-                .toArray(String[]::new);
+    public String[] findWords(String[] words) {
+        String a="qwertyuiopQWERTYUIOP";
+        String b="asdfghjklASDFGHJKL";
+        String c="zxcvbnmZXCVBNM";
+        List<String> l=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            int p=0,q=0,r=0;
+            for(int j=0;j<words[i].length();j++){
+                if(a.indexOf(words[i].charAt(j))!=-1){
+                    p++;
+                }
+                else if(b.indexOf(words[i].charAt(j))!=-1){
+                    q++;
+                }
+                else{
+                    r++;
+                }
+            }
+            if((p==0 && q==0) || (q==0 && r==0) || (p==0 && r==0)){
+                l.add(words[i]);
+            }
+        }
+        String arr[]=new String[l.size()];
+        for(int i=0;i<l.size();i++){
+            arr[i]=l.get(i);
+        }
+        return arr;
     }
 }
