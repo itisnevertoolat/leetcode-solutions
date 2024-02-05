68324ae9801ab7814e31975a595a8f518b7f68a8
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-02-05 23:06:07 +0200

    Time: 31 ms (44.71%), Space: 61.5 MB (71.23%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -3,13 +3,8 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         int[] copied = Arrays.copyOf(arr, arr.length);
         Arrays.sort(copied);
-        for(int x: copied){
-            if(map.containsKey(x))
-                continue;
-            else
-                map.put(x, map.size() + 1);
-            
-        }
+        for(int x: copied)
+            map.putIfAbsent(x, map.size() + 1);
         for(int i=0;i< arr.length;i++)
             copied[i] = map.get(arr[i]);
 
