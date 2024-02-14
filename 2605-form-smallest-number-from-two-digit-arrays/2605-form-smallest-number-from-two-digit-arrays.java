7016ec0fa7bded89331bce0e0c1bbec02e0ad063
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-02-14 21:41:53 +0200

    Time: 2 ms (39.25%), Space: 41.9 MB (14.90%) - LeetHub

--- 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.java
+++ 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.java
@@ -2,16 +2,17 @@ class Solution {
     public int minNumber(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
         StringBuilder sb = new StringBuilder();
-        for(int i: nums1)
-            map.put(i, map.getOrDefault(i, 0)+1);
-        for(int i: nums2)
-            map.put(i, map.getOrDefault(i, 0)+1);
-        for(int i=1;i<=9;i++){
-            if(map.containsKey(i) && map.get(i) == 2)
-                return i;
-        }
         Arrays.sort(nums1);
         Arrays.sort(nums2);
+        for(int i: nums1)
+            map.put(i, map.getOrDefault(i, 0)+1);
+        for(int i: nums2){
+            int x = map.getOrDefault(i, 0)+1;
+            if( x== 2)
+                return i;
+            map.put(i, x);
+        }
+
         
         sb.append(Math.min(nums1[0], nums2[0]));
         sb.append(Math.max(nums1[0], nums2[0]));
