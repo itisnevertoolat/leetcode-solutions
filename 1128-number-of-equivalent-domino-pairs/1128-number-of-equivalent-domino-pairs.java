920f6176b0122e2485c142d06d784d9a15b94795
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-02-15 10:09:06 +0200

    Time: 12 ms (67.36%), Space: 52 MB (97.63%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.java
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.java
@@ -1,15 +1,15 @@
 class Solution {
     public int numEquivDominoPairs(int[][] dominoes) {
+        Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
-        for(int i=0;i<dominoes.length;i++){
-            int a  = dominoes[i][0];
-            int b  =  dominoes[i][1];
-            for(int j=i+1;j<dominoes.length;j++){
-                int c = dominoes[j][0];
-                int d = dominoes[j][1];
-                if((a == c && b == d) || (a == d && b == c))
-                    count++;
-            }
+        for(int[] arr: dominoes){
+            int max = Math.max(arr[0], arr[1]);
+            int min = Math.min(arr[0], arr[1]);
+            int num = min * 10 + max;
+            int mapValue = map.getOrDefault(num , 0);
+            count += mapValue;
+            map.put(num, mapValue+1);
+            
         }
         
         return count;
