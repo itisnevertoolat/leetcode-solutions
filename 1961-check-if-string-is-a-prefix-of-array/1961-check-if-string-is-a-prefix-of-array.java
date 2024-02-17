6b095f845fe62250a58c132c48dce5c8b01cfbf5
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-02-17 22:07:01 +0200

    Time: 1 ms (87.77%), Space: 42.1 MB (87.77%) - LeetHub

--- 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.java
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.java
@@ -1,48 +1,19 @@
 class Solution {
     public boolean isPrefixString(String s, String[] words) {
-        int index = 0;
-        int windex = 0;
-        int r = 0;
-
-        while (index < s.length()) {
-
-            if (r == words.length) {
-                break;
-            }
-
-            if (s.charAt(index++) != words[r].charAt(windex++)) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        while(sb.length() < s.length() && count < words.length)
+            sb.append(words[count++]);
+        if(sb.length() != s.length())
+            return false;
+        int start = 0;
+        int end = s.length() - 1;
+        while(start <= end){
+            if(s.charAt(start) != sb.charAt(start) || s.charAt(end) != sb.charAt(end))
                 return false;
-            }
-
-            if (index == s.length() && r < words.length && windex != words[r].length()) {
-                return false;
-            }
-
-            if (windex == words[r].length()) {
-                windex = 0;
-                r++;
-            }
+            start++;
+            end--;
         }
-        return index == s.length();
+        return true;
     }
 }
-
-// class Solution {
-//     public boolean isPrefixString(String s, String[] words) {
-//         StringBuilder sb = new StringBuilder();
-//         int count = 0;
-//         while(sb.length() < s.length() && count < words.length)
-//             sb.append(words[count++]);
-//         if(sb.length() != s.length())
-//             return false;
-//         int start = 0;
-//         int end = s.length() - 1;
-//         while(start <= end){
-//             if(s.charAt(start) != sb.charAt(start) || s.charAt(end) != sb.charAt(end))
-//                 return false;
-//             start++;
-//             end--;
-//         }
-//         return true;
-//     }
-// }
