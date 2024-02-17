e10cf6458e1da2e3783bda42ecec02f2a1753f63
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-02-17 22:05:50 +0200

    Time: 1 ms (87.77%), Space: 43 MB (9.72%) - LeetHub

--- 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.java
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.java
@@ -1,19 +1,32 @@
 class Solution {
     public boolean isPrefixString(String s, String[] words) {
+        
         StringBuilder sb = new StringBuilder();
-        int count = 0;
-        while(sb.length() < s.length() && count < words.length)
-            sb.append(words[count++]);
-        if(sb.length() != s.length())
-            return false;
-        int start = 0;
-        int end = s.length() - 1;
-        while(start <= end){
-            if(s.charAt(start) != sb.charAt(start) || s.charAt(end) != sb.charAt(end))
-                return false;
-            start++;
-            end--;
+
+        for (String w: words){
+            sb.append(w);
+            if (s.equals(sb.toString())) return true;
         }
-        return true;
+
+        return false;
     }
 }
+// class Solution {
+//     public boolean isPrefixString(String s, String[] words) {
+//         StringBuilder sb = new StringBuilder();
+//         int count = 0;
+//         while(sb.length() < s.length() && count < words.length)
+//             sb.append(words[count++]);
+//         if(sb.length() != s.length())
+//             return false;
+//         int start = 0;
+//         int end = s.length() - 1;
+//         while(start <= end){
+//             if(s.charAt(start) != sb.charAt(start) || s.charAt(end) != sb.charAt(end))
+//                 return false;
+//             start++;
+//             end--;
+//         }
+//         return true;
+//     }
+// }
