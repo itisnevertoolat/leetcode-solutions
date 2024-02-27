6ec4e26ec46c2f84ad4c4c13e3300ac64a7f3b86
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-02-27 21:26:02 +0200

    Time: 24 ms (18.92%), Space: 45.6 MB (61.08%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -3,14 +3,17 @@ class Solution {
         Arrays.sort(nums);
         Map<Integer, Integer> map = new HashMap<>();
         int res = 0;
-        for(int i: nums)
-            map.put(i, map.getOrDefault(i, 0)+1);
-        for(int i=1;i<nums.length;i++){
-            int a = nums[i];
-            int b = nums[i-1];
-            if(a == b)
-                continue;
-            else if(a - (b) == 1){
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            int x = map.getOrDefault(nums[i], 0);
+            if(x == 0)
+                list.add(i);
+            map.put(nums[i],x+1);
+        }
+        for(int i=1;i<list.size();i++){
+            int a = nums[list.get(i)];
+            int b = nums[list.get(i-1)];
+            if(a - (b) == 1){
                 int x = map.get(a) + map.get(b);
                 res = Math.max(res, x);
             }
