b3e93bfccf8471e41ea8c846d049139484587f6f
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-03-06 00:37:51 +0200

    Time: 4 ms (79.81%), Space: 45.2 MB (54.04%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -1,37 +1,34 @@
-class Solution {
-    public int minimumLength(String s) {
-            int i = 0, j = s.length() - 1;
-            while (i < j && s.charAt(i) == s.charAt(j)) {
-                char c = s.charAt(j);
-                while (i <= j && s.charAt(i) == c) {
-                    ++i;
-                }
-                while (i <= j && s.charAt(j) == c) {
-                    --j;
-                }
-            }
-            return j - i + 1;
-        }
-}
 // class Solution {
 //     public int minimumLength(String s) {
-//         if(s.charAt(0) != s.charAt(s.length() - 1))
-//             return s.length();
-//         int size = s.length() - 1;
-//         int start = 0;
-//         int end = size;
-//         while(start < end){
-//             if(s.charAt(start) == s.charAt(end)){
-//                 while(start < end && s.charAt(start) == s.charAt(start + 1))
-//                     start++;
-//                 while(start < end && s.charAt(end) == s.charAt(end - 1))
-//                     end--;
-//                 start++;
-//                 end--;
+//             int i = 0, j = s.length() - 1;
+//             while (i < j && s.charAt(i) == s.charAt(j)) {
+//                 char c = s.charAt(j);
+//                 while (i <= j && s.charAt(i) == c) {
+//                     ++i;
+//                 }
+//                 while (i <= j && s.charAt(j) == c) {
+//                     --j;
+//                 }
 //             }
-//             else
-//                 break;
+//             return j - i + 1;
 //         }
-//         return (end - start) < 0 ? 0: (end - start) + 1;
-//     }
 // }
+class Solution {
+    public int minimumLength(String s) {
+        int size = s.length() - 1;
+        int start = 0;
+        int end = size;
+        while(start < end){
+            char c = s.charAt(end);
+            if(s.charAt(start) == s.charAt(end)){
+                while(start <= end && s.charAt(start) == c)
+                    ++start;
+                while(start <= end && s.charAt(end) == c)
+                    --end;
+            }
+            else
+                break;
+        }
+        return (end - start) + 1;
+    }
+}
