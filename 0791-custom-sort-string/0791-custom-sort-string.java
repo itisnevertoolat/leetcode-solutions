6d97753566d5d4b31febf6ae274e8c02c9839f69
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-03-11 21:26:14 +0200

    Time: 3 ms (36.64%), Space: 41.5 MB (49.15%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,34 +1,33 @@
 class Solution {
     public String customSortString(String order, String s) {
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Integer, Character> map = new HashMap<>();
         Map<Character, Integer> map2 = new HashMap<>();
-        Set<Character> set = new HashSet<>();
-        char[] arr = new char[s.length()];
+        StringBuilder sb = new StringBuilder();
+        
         for(int i=0;i<order.length();i++)
-            map.put(order.charAt(i), i);
+            map.put(i, order.charAt(i));
+        
         for(char c: s.toCharArray()){
             int x = map2.getOrDefault(c ,0)+1;
             map2.put(c, x);
         }
             
-        int count = 1;
-        for(char c: s.toCharArray()){
-            if(map.containsKey(c)){
-                arr[map.get(c)] = c;
-            }
-                
-            else
-                arr[arr.length - count++] = c;
-        }
-        String res = new String(arr).replaceAll("\u0000", "");
-        StringBuilder sb = new StringBuilder();
-        for(char c: res.toCharArray()){
-            if(!set.contains(c)){
+        
+        for(int i=0;i<map.size();i++){
+            char c = map.get(i);
+            if(map2.containsKey(c)){
                 sb.append(c);
                 sb.append((c+"").repeat(map2.get(c)-1));
+                map2.remove(c);
+                
             }
+        }
+        
+        
+        for(char c: map2.keySet()){
+                sb.append(c);
+                sb.append((c+"").repeat(map2.get(c)-1));
 
-            set.add(c);
         }
        
         
