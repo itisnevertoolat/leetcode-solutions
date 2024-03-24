493b88f50eca8eb8488f9ea2c1f6a9f96f990415
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-03-24 10:48:10 +0200

    Time: 4 ms (85.61%), Space: 58.4 MB (37.91%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,5 +1,54 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        if (nums.length > 1)
+        {
+            int slow = nums[0];
+            int fast = nums[nums[0]];
+            while (slow != fast)
+            {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+
+            fast = 0;
+            while (fast != slow)
+            {
+                fast = nums[fast];
+                slow = nums[slow];
+            }
+            return slow;
+        }
+        return -1;
+    }
+}
+
+/*
+    int findDuplicate3(vector<int>& nums)
+{
+	if (nums.size() > 1)
+	{
+		int slow = nums[0];
+		int fast = nums[nums[0]];
+		while (slow != fast)
+		{
+			slow = nums[slow];
+			fast = nums[nums[fast]];
+		}
+
+		fast = 0;
+		while (fast != slow)
+		{
+			fast = nums[fast];
+			slow = nums[slow];
+		}
+		return slow;
+	}
+	return -1;
+}
+
+
+class Solution {
+    public int findDuplicate(int[] nums) {
         Arrays.sort(nums);
         int slow = 0;
         int fast = 1;
@@ -10,3 +59,4 @@ class Solution {
         return 0;
     }
 }
+*/
