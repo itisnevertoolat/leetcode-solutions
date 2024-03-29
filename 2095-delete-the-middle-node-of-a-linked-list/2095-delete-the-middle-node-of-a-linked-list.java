6730e0dade30edc1bce577b486bf702f73ea0e9d
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-03-29 19:35:28 +0200

    Time: 6 ms (6.32%), Space: 68.9 MB (5.74%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -8,6 +8,30 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head.next ==null)
+            return null;
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode res = new ListNode(head.val);
+        ListNode temp = res;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast != null && fast.next != null){
+                temp.next = new ListNode(slow.val);
+                temp = temp.next;
+            }
+
+            
+        }
+        temp.next = slow.next;
+        return res;
+    }
+}
+/*
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
         System.gc();
@@ -40,3 +64,4 @@ class Solution {
         return res;
     }
 }
+*/
