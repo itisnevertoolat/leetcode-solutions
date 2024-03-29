d97ddba023dc55a9727b986b896fd607326bc6d8
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-03-29 22:10:16 +0200

    Time: 0 ms (100.00%), Space: 42.3 MB (12.07%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -8,43 +8,72 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        if(head == null || head.next == null)
-            return head;
-        List<Integer> list = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        ListNode tmp = head;
-        while(tmp != null){
-            if(tmp.val < x)
-                list.add(tmp.val);
-            tmp = tmp.next;
+        ListNode left = new ListNode(0);
+        ListNode right = new ListNode(0);
+        
+        ListNode leftTail = left;
+        ListNode rightTail = right;
+        
+        while(head != null){
+            if(head.val < x){
+                leftTail.next = head;
+                leftTail = leftTail.next;
+            }
+            else{
+                rightTail.next = head;
+                rightTail = rightTail.next;
+            }
+            head = head.next;
         }
-        tmp = head;
-        while(tmp != null){
-            if(tmp.val >= x)
-                list2.add(tmp.val);
-            tmp = tmp.next;
-        }
-        ListNode res;
-        int i = 0;
-        int j = 0;
-        if(list.size() > 0){
-            res = new ListNode(list.get(0));
-            i++;
-        }else{
-            res = new ListNode(list2.get(0));
-            j++;
-        }
-        tmp = res;
-        for(;i<list.size();i++){
-            tmp.next = new ListNode(list.get(i));
-            tmp = tmp.next;
-        }
-        for(;j<list2.size();j++){
-            tmp.next = new ListNode(list2.get(j));
-            tmp = tmp.next;
-        }
-        return res;
+        
+        leftTail.next = right.next;
+        rightTail.next = null;
+        
+        return left.next;
     }
 }
+
+// class Solution {
+//     public ListNode partition(ListNode head, int x) {
+//         if(head == null || head.next == null)
+//             return head;
+//         List<Integer> list = new ArrayList<>();
+//         List<Integer> list2 = new ArrayList<>();
+//         ListNode tmp = head;
+//         while(tmp != null){
+//             if(tmp.val < x)
+//                 list.add(tmp.val);
+//             tmp = tmp.next;
+//         }
+//         tmp = head;
+//         while(tmp != null){
+//             if(tmp.val >= x)
+//                 list2.add(tmp.val);
+//             tmp = tmp.next;
+//         }
+//         ListNode res;
+//         int i = 0;
+//         int j = 0;
+//         if(list.size() > 0){
+//             res = new ListNode(list.get(0));
+//             i++;
+//         }else{
+//             res = new ListNode(list2.get(0));
+//             j++;
+//         }
+//         tmp = res;
+//         for(;i<list.size();i++){
+//             tmp.next = new ListNode(list.get(i));
+//             tmp = tmp.next;
+//         }
+//         for(;j<list2.size();j++){
+//             tmp.next = new ListNode(list2.get(j));
+//             tmp = tmp.next;
+//         }
+//         return res;
+//     }
+// }
