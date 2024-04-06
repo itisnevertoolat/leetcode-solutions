c694bb0827a3e9b3133ec05f30da91ff1828b431
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-04-06 16:48:47 +0200

    Time: 5 ms (71.86%), Space: 49.2 MB (35.93%) - LeetHub

--- 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.java
+++ 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.java
@@ -1,20 +1,29 @@
 class Solution {
     public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
         int[] res = new int[nums.length];
-        int count = 0;
-        for(int[] query : queries){
-            nums[query[1]] = nums[query[1]] + query[0];
-            res[count++] = evenCal(nums);
+        int count = 0, sum =  0;
+        for(int i: nums)
+            sum += i % 2 == 0 ? i : 0;
+        
+        for(int[] query : queries){            
+            if(nums[query[1]] % 2 == 0)
+                sum -= nums[query[1]];
+            
+             nums[query[1]] += query[0];
+            
+            if(nums[query[1]] % 2 ==0)
+                sum += nums[query[1]];
+            res[count++] = sum;
         }
         return res;
         
     }
-    public int evenCal(int[] arr){
-        int sum =  0;
-        for(int i: arr)
-            if(i % 2 == 0)
-                sum += i;
-        return sum;
-    }
+    // public int evenCal(int[] arr){
+    //     int sum =  0;
+    //     for(int i: arr)
+    //         if(i % 2 == 0)
+    //             sum += i;
+    //     return sum;
+    // }
     
 }
