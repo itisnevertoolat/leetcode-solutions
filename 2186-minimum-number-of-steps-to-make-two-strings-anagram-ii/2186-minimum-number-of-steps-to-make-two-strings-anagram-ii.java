f57f4786cd682081ecc524d1530bed524fdecb5c
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-04-22 23:21:56 +0200

    Time: 116 ms (13.27%), Space: 51.9 MB (7.48%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.java
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.java
@@ -1,22 +1,17 @@
 class Solution {
     public int minSteps(String s, String t) {
         Map<Character, Integer> map =  new HashMap<>();
-        Map<Character, Integer> map2 =  new HashMap<>();
-        Set<Character> set = new HashSet<>();
         int res = 0;
-        String word = "zxcvbnmasdfghjklqwertyuiop";
-        for(char c : s.toCharArray()){
+        for(char c : s.toCharArray())
             map.put(c, map.getOrDefault(c, 0)+1);
-            set.add(c);
-        }
             
-        for(char c : t.toCharArray()){
-            map2.put(c, map2.getOrDefault(c, 0)+1);
-            set.add(c);
-        }
-            
-        for(char c: set)
-            res += Math.abs(map.getOrDefault(c, 0) - map2.getOrDefault(c, 0));
+        for(char c : t.toCharArray())
+            map.put(c, map.getOrDefault(c, 0)-1);
+        
+        for(char c: map.keySet())
+            res += Math.abs(map.get(c));
+        // for(char c: set)
+        //     res += Math.abs(map.getOrDefault(c, 0) - map2.getOrDefault(c, 0));
         
         return res;
     }
