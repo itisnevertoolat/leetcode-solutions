5498f0ee688d2d4fd15a2f605e736c0141897dee
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-04-28 21:26:47 +0300

    Time: 57 ms (33.61%), Space: 63 MB (20.70%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
@@ -1,20 +1,44 @@
 class Solution {
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> list  = new ArrayList<>();
-         for(int i: arr)
-             map.put(i, map.getOrDefault(i, 0)+1);
-        for(Integer key : map.keySet())
-            list.add(map.get(key));
-        Collections.sort(list);
-        while(k > 0){
-            k -=list.get(0);
-            if(k >= 0)
-                list.remove(0);
-            
+        for (int x : arr) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
         }
-        return list.size();
-        
-        
+
+        List<Map.Entry<Integer, Integer>> elements = new ArrayList<>(map.entrySet());
+
+        elements.sort((a, b) -> a.getValue() - b.getValue());
+
+        for (Map.Entry<Integer, Integer> entry : elements) {
+            if (entry.getValue() <= k) {
+                k -= entry.getValue();
+                map.remove(entry.getKey());
+            } else {
+                break;
+            }
+        }
+        return map.size();
     }
 }
+
+
+// class Solution {
+//     public int findLeastNumOfUniqueInts(int[] arr, int k) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         List<Integer> list  = new ArrayList<>();
+//          for(int i: arr)
+//              map.put(i, map.getOrDefault(i, 0)+1);
+//         for(Integer key : map.keySet())
+//             list.add(map.get(key));
+//         Collections.sort(list);
+//         while(k > 0){
+//             k -=list.get(0);
+//             if(k >= 0)
+//                 list.remove(0);
+            
+//         }
+//         return list.size();
+        
+        
+//     }
+// }
