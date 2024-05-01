6eac186e4236e56ffb7c08293bbd0af263ad4b47
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-05-01 22:38:40 +0300

    Time: 7 ms (97.54%), Space: 53.7 MB (21.19%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int equalPairs(int[][] grid) {
+        int size = grid[0].length;
+        int[][] columns = new int[size][size];
+        for(int i=0;i<size;i++){
+            int[] column = new int[size];
+            for(int j=0;j<size;j++){
+                column[j] = grid[j][i];
+            }
+            columns[i] = column;
+        }
+        int  res = 0;
+        for(int[] row: grid){
+            for(int[] col: columns){
+                if(Arrays.equals(row, col))
+                    res++;
+            }
+        }
+        return res;
+        
+    }
+}
