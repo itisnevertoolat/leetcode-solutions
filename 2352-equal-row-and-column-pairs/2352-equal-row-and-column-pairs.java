ffe50c15742b1fde14179a47e597107950d27172
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-05-01 22:40:09 +0300

    Time: 9 ms (90.34%), Space: 53.7 MB (21.19%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,14 +1,6 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        int size = grid[0].length;
-        int[][] columns = new int[size][size];
-        for(int i=0;i<size;i++){
-            int[] column = new int[size];
-            for(int j=0;j<size;j++){
-                column[j] = grid[j][i];
-            }
-            columns[i] = column;
-        }
+        int[][] columns = getColumns(grid);
         int  res = 0;
         for(int[] row: grid){
             for(int[] col: columns){
@@ -19,4 +11,43 @@ class Solution {
         return res;
         
     }
+    
+    public int[][] getColumns(int[][] grid){
+        int size = grid[0].length;
+        int[][] columns = new int[size][size];
+        for(int i=0;i<size;i++){
+            int[] column = new int[size];
+            for(int j=0;j<size;j++){
+                column[j] = grid[j][i];
+            }
+            columns[i] = column;
+        }
+        return columns;
+        
+    }
 }
+
+
+
+// class Solution {
+//     public int equalPairs(int[][] grid) {
+//         int size = grid[0].length;
+//         int[][] columns = new int[size][size];
+//         for(int i=0;i<size;i++){
+//             int[] column = new int[size];
+//             for(int j=0;j<size;j++){
+//                 column[j] = grid[j][i];
+//             }
+//             columns[i] = column;
+//         }
+//         int  res = 0;
+//         for(int[] row: grid){
+//             for(int[] col: columns){
+//                 if(Arrays.equals(row, col))
+//                     res++;
+//             }
+//         }
+//         return res;
+        
+//     }
+// }
