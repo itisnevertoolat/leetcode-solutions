8d7803f275d80be4dd9cfd18195ced90649902e2
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-11-01 22:47:31 +0200

    Time: 3 ms (35.35%), Space: 44.6 MB (84.00%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
@@ -1,22 +1,28 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
         int[] res = new int[nums.length];
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i: nums)
-            pq.add(i);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new CustomComparator());
+         for(int x=0;x<nums.length;x++)
+            pq.add(new int[]{nums[x], x});
         while(k-- >= 1){
-            int num = pq.poll();
-            int multiNum = num * multiplier;
-            pq.add(multiNum);
-            for(int x=0;x<nums.length;x++){
-                if(nums[x] == num){
-                    nums[x] = multiNum;
-                    break;
-                }
-            }
+            int[] num = pq.poll();
+            int multiNum = num[0] * multiplier;
+            pq.add(new int[]{multiNum, num[1]});
+            nums[num[1]] = multiNum;
             
         }
         
         return nums;
     }
+}
+
+class CustomComparator implements Comparator<int[]>{
+             
+        public int compare(int[] arr1, int[] arr2) {
+            if ( arr1[0]> arr2[0])
+                return 1;
+            else if (arr1[0] < arr2[0])
+                return -1;
+            return arr1[1] - arr2[1];
+            }
 }
