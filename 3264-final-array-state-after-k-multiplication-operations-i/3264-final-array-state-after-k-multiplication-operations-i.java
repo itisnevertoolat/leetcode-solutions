915b660509955d6b7d6386bcd945bc10ac8c0e89
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-11-01 22:48:18 +0200

    Time: 4 ms (19.19%), Space: 44.7 MB (72.29%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
@@ -26,3 +26,27 @@ class CustomComparator implements Comparator<int[]>{
             return arr1[1] - arr2[1];
             }
 }
+
+
+// class Solution {
+//     public int[] getFinalState(int[] nums, int k, int multiplier) {
+//         int[] res = new int[nums.length];
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+//         for(int i: nums)
+//             pq.add(i);
+//         while(k-- >= 1){
+//             int num = pq.poll();
+//             int multiNum = num * multiplier;
+//             pq.add(multiNum);
+//             for(int x=0;x<nums.length;x++){
+//                 if(nums[x] == num){
+//                     nums[x] = multiNum;
+//                     break;
+//                 }
+//             }
+            
+//         }
+        
+//         return nums;
+//     }
+// }
