93b83f15af3a72322acd5bb813e58d35f19a1b7e
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-11-03 23:13:11 +0200

    Time: 8 ms (91.48%), Space: 41.8 MB (21.95%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.java
+++ 0355-design-twitter/0355-design-twitter.java
@@ -1,95 +1,53 @@
-public class Twitter {
-    Map<Integer, Set<Integer>> fans = new HashMap<>();
-    Map<Integer, LinkedList<Tweet>> tweets = new HashMap<>();
-    int cnt = 0;
+import java.util.*;
 
-    public void postTweet(int userId, int tweetId) {
-        if (!fans.containsKey(userId)) fans.put(userId, new HashSet<>());
-        fans.get(userId).add(userId);
-        if (!tweets.containsKey(userId)) tweets.put(userId, new LinkedList<>());
-        tweets.get(userId).addFirst(new Tweet(cnt++, tweetId));
+class Twitter {
+    Map<Integer, int[]> tweets = new HashMap<>();
+    Map<Integer, List<Integer>> followers = new LinkedHashMap<>();
+    int count = 1;
+    public Twitter() {
+        
     }
-
+    
+    public void postTweet(int userId, int tweetId) {
+        tweets.put(count++, new int[]{userId, tweetId});
+    }
+    
     public List<Integer> getNewsFeed(int userId) {
-        if (!fans.containsKey(userId)) return new LinkedList<>();
-        PriorityQueue<Tweet> feed = new PriorityQueue<>((t1, t2) -> t2.time - t1.time);
-        fans.get(userId).stream()
-            .filter(f -> tweets.containsKey(f))
-            .forEach(f -> tweets.get(f).forEach(feed::add));
-        List<Integer> res = new LinkedList<>();
-        while (feed.size() > 0 && res.size() < 10) res.add(feed.poll().id);
+        List<Integer> dates = new ArrayList<>();
+        for(Map.Entry<Integer, int[]> entry: tweets.entrySet())
+            dates.add(entry.getKey());
+        dates = dates.reversed();
+        List<Integer> res = new ArrayList<>();
+        int countOfTweets = 1;
+        
+        for(Integer d : dates){
+            if(countOfTweets == 11)
+                break;
+            int[] tweet = tweets.get(d);
+            if(tweet[0] == userId || 
+                (followers.get(userId) != null && followers.get(userId).contains(tweet[0]))
+               ){
+                countOfTweets++;
+                res.add(tweets.get(d)[1]);
+            }
+                 
+        }
         return res;
     }
-
+    
     public void follow(int followerId, int followeeId) {
-        if (!fans.containsKey(followerId)) fans.put(followerId, new HashSet<>());
-        fans.get(followerId).add(followeeId);
+        followers.computeIfAbsent(followerId, k -> new ArrayList<>());
+        followers.get(followerId).add(followeeId);
     }
-
+    
     public void unfollow(int followerId, int followeeId) {
-        if (fans.containsKey(followerId) && followeeId != followerId) fans.get(followerId).remove(followeeId);
-    }
-
-    class Tweet {
-        int time;
-        int id;
-
-        Tweet(int time, int id) {
-            this.time = time;
-            this.id = id;
+        if(followers.get(followerId) != null && followers.get(followerId).contains(followeeId)){
+            int idx = followers.get(followerId).indexOf(followeeId);
+            followers.get(followerId).remove(idx);
         }
+
     }
 }
-// import java.util.*;
-
-// class Twitter {
-//     Map<Integer, int[]> tweets = new HashMap<>();
-//     Map<Integer, List<Integer>> followers = new LinkedHashMap<>();
-//     int count = 1;
-//     public Twitter() {
-        
-//     }
-    
-//     public void postTweet(int userId, int tweetId) {
-//         tweets.put(count++, new int[]{userId, tweetId});
-//     }
-    
-//     public List<Integer> getNewsFeed(int userId) {
-//         List<Integer> dates = new ArrayList<>();
-//         for(Map.Entry<Integer, int[]> entry: tweets.entrySet())
-//             dates.add(entry.getKey());
-//         dates = dates.reversed();
-//         List<Integer> res = new ArrayList<>();
-//         int countOfTweets = 1;
-        
-//         for(Integer d : dates){
-//             if(countOfTweets == 11)
-//                 break;
-//             int[] tweet = tweets.get(d);
-//             if(tweet[0] == userId || 
-//                 (followers.get(userId) != null && followers.get(userId).contains(tweet[0]))
-//                ){
-//                 countOfTweets++;
-//                 res.add(tweets.get(d)[1]);
-//             }
-                 
-//         }
-//         return res;
-//     }
-    
-//     public void follow(int followerId, int followeeId) {
-//         followers.computeIfAbsent(followerId, k -> new ArrayList<>());
-//         followers.get(followerId).add(followeeId);
-//     }
-    
-//     public void unfollow(int followerId, int followeeId) {
-//         if(followers.get(followerId) != null && followers.get(followerId).contains(followeeId)){
-//             int idx = followers.get(followerId).indexOf(followeeId);
-//             followers.get(followerId).remove(idx);
-//         }
-
-//     }
-// }
 
 /**
  * Your Twitter object will be instantiated and called as such:
