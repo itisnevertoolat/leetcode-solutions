5e11de11bc4182538f77999ddce1834a674feb05
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-11-13 20:31:22 +0200

    Time: 53 ms (56.52%), Space: 57.4 MB (26.26%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -1,40 +1,31 @@
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
-        
         Arrays.sort(nums);
         long res = 0;
-        int right = nums.length - 1;
-        int lastIdx = nums.length - 1;
-        for(int i=0;i<nums.length;i++){
-            int left = i;
-            
-            int largeIdx = 0;
-            int smallIdx = 0;
-            while(right > i){
-                int num = nums[i] + nums[right];
-                if(num > upper)
-                    right--;
-                else{
-                    largeIdx = right;
-                    break;
-                }                    
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] + nums[i] > upper) {
+                break;
             }
-            left = i+1;
-            if(left > lastIdx)
-                smallIdx = left;
-            while(left <= lastIdx){
-                int num = nums[i] + nums[left];
-                if(num < lower)
-                    left++;
-                else{
-                    smallIdx = left;
-                    break;
-                }                    
-            }
-            if(smallIdx != 0 && largeIdx != 0)
-                res += (largeIdx  - smallIdx) + 1;
-            
-    }
+            int largeIdx = binarySearch(nums, i + 1, nums.length - 1, nums[i], upper);
+
+            int smallIdx = binarySearch(nums, i + 1, nums.length - 1, nums[i], lower - 1);
+
+            res += largeIdx - smallIdx;
+        }
+
         return res;
+    }
+
+    private int binarySearch(int[] nums, int left, int right, int target, int upperBound) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] + target <= upperBound) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left - 1;
     }
 }
