621e646a5f53a611065ac6210a542a274b9c39c8
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-11-20 22:36:25 +0200

    Time: 11 ms (19.53%), Space: 44.6 MB (89.65%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
@@ -2,7 +2,7 @@ class Solution {
     public int largestPerimeter(int[] nums) {
         Arrays.sort(nums);
         int ans = 0;
-        for(int i=2;i<nums.length;i++){
+        for(int i=nums.length-1;i >= 2;i--){
             int sum = nums[i-2] + nums[i-1];
             if(sum > nums[i]){
                 int perimeter = sum + nums[i];
