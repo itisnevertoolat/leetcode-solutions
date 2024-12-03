8a734d0c4abf77626445a8ccb1e09deccda4ca34
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-12-03 13:05:38 +0200

    Time: 1 ms (100.00%), Space: 42.2 MB (29.20%) - LeetHub

--- 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.java
+++ 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.java
@@ -2,22 +2,16 @@ class Solution {
     public int distMoney(int money, int children) {
         if(money < children)
             return -1;
+        if(money > children * 8) return children - 1;
         int res = 0;
-        while(children > 0 && money >  0){
-            if(money - 8 >= 0){
-                if(children == 2 && money - 8 == 4)
-                    return res;
-                if (money -8 < (children - 1) || ((children -1 ) == 0 && (money -8) != 0))
-                    return res;
-                    
-                res++;
-                money -= 8;
-                children--;
-            }else{
-                children--;
-            }
+        while(money >  0 && money -8 >= children - 1){
+            res++;
+            money -=8;
+            children--;
                 
         }
+        if(money == 4 && children == 1)
+            return --res;
         return res;
     }
 }
