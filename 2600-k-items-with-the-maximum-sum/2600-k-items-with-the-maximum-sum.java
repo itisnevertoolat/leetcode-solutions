f895d7bd484f54102e03b1852ddbb571ca99aac9
Muhammed Ahmed <131061525+itisnevertoolat@users.noreply.github.com>
2024-12-03 13:52:24 +0200

    Time: 1 ms (91.37%), Space: 41.1 MB (61.87%) - LeetHub

--- 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
@@ -1,29 +1,41 @@
 class Solution {
     public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
-        int res = 0;
-        while(k > 0){
-            if(k > numOnes && numOnes != 0){
-                k -= numOnes;
-                res += numOnes;
-                numOnes = 0;
-            }
-            else if(numOnes >= k)
-                return k;
-            else if(k > numZeros && numZeros != 0){
-                k -= numZeros;
-                numZeros = 0;
-            }
-            else if(numZeros >= k)
-                return res;
-            else if(k > numNegOnes && numNegOnes != 0 ){
-                k -= numNegOnes;
-                numNegOnes = 0;
-                res -= numNegOnes;
-            }
-            else if(numNegOnes >= k)
-                return res -= k;
+        int total = 0;
+        
+        int select=Math.min(numOnes,k);       
+		total+=select;                        
+        k-=select;                                  
+        
+        select=Math.min(numZeros,k);          
+        k-=select;                                 
+        
+        select=Math.min(numNegOnes,k);  
+        total-=select;    
+        
+        return total;
+//         while(k > 0){
+//             if(k > numOnes && numOnes != 0){
+//                 k -= numOnes;
+//                 res += numOnes;
+//                 numOnes = 0;
+//             }
+//             else if(numOnes >= k)
+//                 return k;
+//             else if(k > numZeros && numZeros != 0){
+//                 k -= numZeros;
+//                 numZeros = 0;
+//             }
+//             else if(numZeros >= k)
+//                 return res;
+//             else if(k > numNegOnes && numNegOnes != 0 ){
+//                 k -= numNegOnes;
+//                 numNegOnes = 0;
+//                 res -= numNegOnes;
+//             }
+//             else if(numNegOnes >= k)
+//                 return res -= k;
             
-        }
-        return res;
+//         }
+        // return res;
     }
 }
